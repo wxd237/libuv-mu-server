352fc55e87e3cad581e970726eb7a5c3b2d8a208
wxd237 <wxd237@gmail.com>
2017-05-25 22:47:13 +0800

    haha

---
+++ mainpool.c
@@ -0,0 +1,48 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  mainpool.c
+ *
+ *    Description:  mainpool.c
+ *
+ *        Version:  1.0
+ *        Created:  2017/05/25 22时34分41秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ *      initpool->
+ *           parse the message into pool
+ *           call the tran pre-event
+ *           call the tran handle function
+ *           call the tran after-event 
+ *           return the client the response message
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include "memhash.h"
+
+
+/**
+* @brief  init the pool 
+*/
+void initpool(){
+
+
+}
+
+
+/**
+* @brief   main trade handle 
+*
+* @param message
+*
+* @return 
+*/
+int mainHandle(char * message){
+	
+	return 0;
+}
